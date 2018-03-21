844e4abfb9ac04d2372efe490343f0978bd936ad
luisalfredocrespo <32372666+luisalfredocrespo@users.noreply.github.com>
2018-03-21 01:14:40 -0600

    historia
    historia

--- templet/nosotros.php
+++ templet/nosotros.php
@@ -9,4 +9,21 @@
                             </div>
                         </div>
                    	</div>
+<div class="container"><!-- Container -->
+              <h2 class="border-title aligncenter">
+                   Salon Evolution 
+                </h2>
+                <font color=red><p class="aligncenter">Abre sus puertas el 10 de agosto 2002 en calle 11 # 47 colonia Espartaco Coyoacán y inicia con el nombre de estética capricornio el 22 de octubre cambia de dirección avenida 4 # 87 colonia Espartaco Coyoacán el cual a estado trabajando de la mano con los productos ALFA PARF (ITALIANOS) , tec Italy (ilaliano) , salerm (español) el cual nos accesoramos</p></font>
+        <div class="clear"></div>
+                <div class="hr-invisible-medium"></div>
+                <div class="hr-invisible-very-very-small"></div>
+                <div class="column dt-sc-one-fourth first">
+                  <div class="dt-sc-service type1 animate" data-delay="100" data-animation="fadeIn animated">
+                     
+                        
+                    </div>
+                </div>
+               
+               
+            </div><!-- End of Container -->
              	</div> 				
